67d0561765ca6dcef0738eaeb37ed467aa62864f
aywar2000 <polamekkartnija@gmail.com>
2022-09-07 21:46:02 +0200

    code sketches and basics

--- index.js
+++ index.js
@@ -18,15 +18,71 @@ const devBtn = document.getElementById('development')
 //
 const widgetBtn = document.getElementById('widget-btn')
 const widgetEl = document.getElementById('widget')
+const widgetFontBtn = document.getElementById('widget-font')
+const widgetLineBtn = document.getElementById('widget-line')
+const widgetFontSize = document.querySelector('.widget__font-size')
+const widgetLineHeight = document.querySelector('.widget__line-height')
+const increaseBtn = document.getElementById('increment')
+const decreaseBtn = document.getElementById('decrement')
+//
+const text = document.querySelectorAll('.text-card__text')
 
-devBtn.addEventListener('click', () => {
-    contentSection.classList.toggle('hidden')
-    devSection.classList.toggle('hidden')
+//
+let isActive = false
+
+//
+// console.log('style', document.getElementsByTagName('p').style.fontSize)
+
+widgetFontBtn.addEventListener('click', ()=> {
+    widgetFontSize.classList.remove('hidden')
+    widgetFontSize.classList.add('active')
+    widgetFontSize.setAttribute('aria-expanded', 'true')
+    isActive = true
+    return isActive
+})
+
+    let startValue = 0.2
+    let fontValue = 0.8
+    increaseBtn.addEventListener('click', ()=> {
+    console.log('here')
+    fontValue = fontValue + startValue
+    console.log('vale', fontValue)
+    for (let j = 0; j < text.length; j++){
+        let textElem = text[j]
+        console.log(textElem.style.fontSize)
+        textElem.style.fontSize = fontValue + 'em'
+    }
+})
+
+
+let decreasedFont
+let decreaseValue = 0.2
+
+
+decreaseBtn.addEventListener('click', ()=> {
+    console.log('in decrease')
+    for (let k = 0; k < text.length; k++){
+        let textElem = text[k]
+        let fontSize = textElem.style.fontSize
+        let newValue = parseInt(fontSize)
+        decreasedFont = newValue - decreaseValue
+        textElem.style.fontSize = decreasedFont + 'em'
+    }
+})
+
+
+window.addEventListener('load', ()=>{
     if(devSection.classList.contains('hidden')) {
         devSection.classList.remove('development')
     }else{
         devSection.classList.add('development')
     }
+})
+
+devBtn.addEventListener('click', () => {
+    contentSection.classList.toggle('hidden')
+    devSection.classList.toggle('hidden')
+
 })
 
 widgetBtn.addEventListener('click', ()=> {
